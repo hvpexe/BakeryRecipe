0cc723d8ab7f983c26c8bb23c2004b418b396b35
hvpexe <phuhuynh923@gmail.com>
2022-09-21 23:49:07 +0700

    edit DBUtils

--- Project/BakeryRecipe/src/main/java/utilities/DBUtils.java
+++ Project/BakeryRecipe/src/main/java/utilities/DBUtils.java
@@ -20,7 +20,7 @@ public class DBUtils {
         try {
             String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
             if (instance == null || instance.trim().isEmpty()) {
-                url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+                url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;";
             }
             url += ";CharacterSet=UTF-8";//important to add UTF-8 format
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -39,4 +39,8 @@ public class DBUtils {
     private final static String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final static String userID = "sa";
     private final static String password = "12345";
+    
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
 }
