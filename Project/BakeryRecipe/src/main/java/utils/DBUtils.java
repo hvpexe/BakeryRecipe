d972efe7448574a52a55d73032c022c129cb590a
hvpexe <phuhuynh923@gmail.com>
2022-09-22 00:13:55 +0700

    edit DBUtils

--- Project/BakeryRecipe/src/main/java/utils/DBUtils.java
+++ Project/BakeryRecipe/src/main/java/utils/DBUtils.java
@@ -22,7 +22,7 @@ public class DBUtils {
             if (instance == null || instance.trim().isEmpty()) {
                 url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
             }
-            url += ";CharacterSet=UTF-8";//important to add UTF-8 format
+            url += ";CharacterSet=UTF-8;encrypt=true;trustServerCertificate=true;";//important to add UTF-8 format
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             conn = DriverManager.getConnection(url, userID, password);
             return conn;
@@ -39,4 +39,7 @@ public class DBUtils {
     private final static String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final static String userID = "sa";
     private final static String password = "12345";
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
 }
