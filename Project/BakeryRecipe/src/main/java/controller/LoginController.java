fcd23e6b69ff768cb5007a08528f9eacda418008
hvpexe <phuhuynh923@gmail.com>
2022-09-25 10:50:42 +0700

    update login

--- Project/BakeryRecipe/src/main/java/controller/LoginController.java
+++ Project/BakeryRecipe/src/main/java/controller/LoginController.java
@@ -22,13 +22,13 @@ import utilities.DBUtils;
  */
 @WebServlet(name = "LoginController", urlPatterns = {"/login"})
 public class LoginController extends HttpServlet {
+
     private static final String ERROR = "login.jsp";
-    private static final String AD = "AD";
-    private static final String AD_PAGE = "adminPage.jsp";
-    private static final String US = "US";
-    private static final String USER_PAGE = "userPage.jsp";
-    private static final String SHOP = "SHOP";
-    private static final String SHOP_PAGE = "shopPage.jsp";
+    private static final String AD = "admin";
+    private static final String AD_PAGE = "homePage.jsp";
+    private static final String US = "baker";
+    private static final String USER_PAGE = "homePage.jsp";
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -40,36 +40,30 @@ public class LoginController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            String url = ERROR;
-            try {
-                String email = request.getParameter("email");
-                String password = request.getParameter("password");
-                UserDAO dao = new UserDAO();
-                User loginUser = dao.login(email, password);
-                if (loginUser != null) {
-                    HttpSession session = request.getSession();
-                    session.setAttribute("LOGIN_USER", loginUser);
-                    String roleID = loginUser.getRole();
-                    Boolean isActive = loginUser.isIsActive();
-                    if (isActive.equals("0")) {
-                        request.setAttribute("loginError", "You have been banned");
-                    } else {
-                        if (AD.equals(roleID)) {
-                            url = AD_PAGE;
-                        } else if (US.equals(roleID)) {
-                            url = USER_PAGE;
-                        } else if (SHOP.equals(roleID)) {
-                            url = SHOP_PAGE;
-                        }
+        String url = ERROR;
+        try {
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            User loginUser = UserDAO.login(email, password);
+            if (loginUser != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("LOGIN_USER", loginUser);
+                String roleID = loginUser.getRole();
+                Boolean isActive = loginUser.isIsActive();
+                if (isActive == false) {
+                    request.setAttribute("loginError", "You have been banned");
+                } else {
+                    if (AD.equals(roleID)) {
+                        url = AD_PAGE;
+                    } else if (US.equals(roleID)) {
+                        url = USER_PAGE;
                     }
                 }
-            } catch (Exception e) {
-                log("Error at LoginController" + e.toString());
-            } finally {
-                request.getRequestDispatcher(url).forward(request, response);
             }
+        } catch (Exception e) {
+            log("Error at LoginController" + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
         }
     }
 
