4a86113e700703deef24cd4b3004c88c2a3e60bb
MooLight <binhntse160860@fpt.edu.vn>
2022-09-25 10:43:01 +0700

    issue with google login
    3 different login with google failed miserably

---
+++ Project/BakeryRecipe/src/main/java/stackjava/com/accessgoogle/servlet/LoginGoogleServlet.java
@@ -0,0 +1,37 @@
+package stackjava.com.accessgoogle.servlet;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import stackjava.com.accessgoogle.common.GooglePojo;
+import stackjava.com.accessgoogle.common.GoogleUtils;
+@WebServlet("/login-google")
+public class LoginGoogleServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  public LoginGoogleServlet() {
+    super();
+  }
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String code = request.getParameter("code");
+    if (code == null || code.isEmpty()) {
+      RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+      dis.forward(request, response);
+    } else {
+      String accessToken = GoogleUtils.getToken(code);
+      GooglePojo googlePojo = GoogleUtils.getUserInfo(accessToken);
+      request.setAttribute("id", googlePojo.getId());
+      request.setAttribute("name", googlePojo.getName());
+      request.setAttribute("email", googlePojo.getEmail());
+      RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
+      dis.forward(request, response);
+    }
+  }
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doGet(request, response);
+  }
+}
