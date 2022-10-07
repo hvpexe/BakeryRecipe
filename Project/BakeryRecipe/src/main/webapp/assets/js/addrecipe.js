6b2f12acae9ef82dfbb2a20860b056f9b3e4cc05
MooLight <98044466+mcgamewebsite@users.noreply.github.com>
2022-10-07 09:44:50 +0700

    addrecipe 30%

--- Project/BakeryRecipe/src/main/webapp/assets/js/addrecipe.js
+++ Project/BakeryRecipe/src/main/webapp/assets/js/addrecipe.js
@@ -9,36 +9,50 @@ ingredentElem.onkeyup = e => {
         var elem = e.target;
         var container = elem.parentElement;
         //jQuery.get( url [, data ] [, success ] [, dataType ] ); 
-        var count = elem.getAttribute('data-count') + 1;
-        var clone = newItem('#item', 'item' + count, (item) => {
-            item = '#' + item;
-            $.ajax({
-                url: "ajax/GetIngredientImage",
-                type: "get", //send it through get method
-                data: {
-                    name: elem.value
-                },
-                success: function (response) {
-                    //Do Something
-                    $(item).html(response);
-                    container.insertBefore(clone, elem);
-                    elem.value = '';
-                    elem.setAttribute('data-count',count);
-                },
-                error: function (xhr) {
-                    //Do Something to handle error
-                    $(item).html('error');
-                }
-            });
-        });
-        //finnally
+        var count = parseInt(elem.dataset.count) + 1;
+        // i want the newItem function to insert in the id and the new ID so that 
+        // i can insert new searched element 
+        var clone = newItem('#item', 'item' + count);
+        $.ajax({
+            url: "ajax/GetIngredientImage",
+            type: "get", //send it through get method
+            data: {
+                name: elem.value
+            },
+            success: function (response) {
+                //Do Something
+                var id = clone.id;
 
+                $(clone.item).html(response);
+                container.insertBefore(clone.item, elem);
+                //img tag
+                $(clone.item).children()[0].onerror = () => {
+                    this.src = "assets/images/ingredients/default.png";
+                }
+                //trashbin tag
+                $(clone.item).children()[2].onclick = (e) => {
+                    e.target.parentElement.remove();
+                }
+                elem.value = '';
+                elem.setAttribute('data-count', count);
+            },
+            error: function (xhr) {
+                //Do Something to handle error
+                $('#test').html('error');
+            }
+        });
         return;
     }
 }
 function newItem(item, newID, test) {
     var item = document.querySelector(item).cloneNode();
     item.setAttribute("id", newID);
-    test(item.id);
-    return item;
+    return {
+        id: '#' + newID,
+        item: item
+    };
 }
+function changeImg(elem, value) {
+    console.log(value);
+    elem.src = value;
+} 
